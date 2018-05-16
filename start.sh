6abe5853fe476ec4ff48987917fc164b4abb28c5
jekshmek <yaroshjeka@gmail.com>
2018-05-16 21:08:42 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 echo "git clone https://github.com/Jekshmek/docker-serv.git" 
 git clone https://github.com/Jekshmek/docker-serv.git
 cd docker-serv/
-echo "sudo docker-compose up -d --build --no-cache" 
-sudo docker-compose up -d  --build --no-cache
+echo "sudo docker-compose up -d --build" 
+sudo docker-compose up -d  --build
 sudo chown $USER:$USER -сR docker-serv/php-code/
 gnome-open http://localhost:8080/
 exit 0
