d490adda52713a175db2790fd0772f08cf547903
jekshmek <yaroshjeka@gmail.com>
2018-05-16 21:09:01 +0300

    Update start_v2.sh

--- start_v2.sh
+++ start_v2.sh
@@ -7,8 +7,8 @@ sudo rm -rf php-code/
 echo "clone project"
 git clone https://github.com/Jekshmek/php-code.git php-code
 
-echo "sudo docker-compose up -d --build --no-cache" 
-#sudo docker-compose up -d  --build --no-cache
+echo "sudo docker-compose up -d --build" 
+#sudo docker-compose up -d  --build
 sudo chown $USER:$USER -c -R docker-serv/php-code/
 #gnome-open http://localhost:8080/
 exit 0
